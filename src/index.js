13b4ccc732005b9b21b2cdd32cdf564eba95afbb
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-16 14:50:17 -0500

    Added console logs for request headers

--- src/index.js
+++ src/index.js
@@ -6,19 +6,15 @@ const Parser = require('./parser')
 const publicDir = path.join(__dirname, '../', 'public')
 const html = path.join(__dirname, '../', 'public', 'index.html')
 
-const getIP = (req,res,next) => {
-    console.log(req.ip)
-    next()
-}
-
 app.use('/', express.static(path.normalize(publicDir)))
 
 app.get('/', (req, res) => {
     res.sendFile(path.normalize(html))
 })
 
-app.get('/whoami', getIP, (req, res) => {
-    console.log(req.ip)
+app.get('/whoami', (req, res) => {
+    console.log(`IP ADDRESS: ${req.ip}`)
+    console.log(`REQUEST HEADERS: ${req.headers}`)
     const parse = new Parser(req)
     res.json(parse.getHash())
 })
